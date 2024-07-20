4e89e7259fdee898e590a0688f59dd10a1469cb9
Ксения Че <chechulina75@list.ru>
2024-07-20 22:46:17 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,13 @@
+a = int(input('first '))
+b = int(input('second '))
+c = int(input('third '))
+
+if a!=b and b!=c and a!=c:
+    print("0")
+elif a == b and b==c and a==c:
+        print("3")
+else:
+        print("2")
+
+
+
